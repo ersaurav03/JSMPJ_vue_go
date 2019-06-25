4259d7a49e3f658813b04d30808909a7dcc6dfff
sauravmaropost <saurav.kumar@maropost.com>
2019-06-25 14:07:57 +0530

    Added handlers

--- GoCode/src/main.go
+++ GoCode/src/main.go
@@ -1,29 +1,33 @@
 package main
 import(
-	"fmt"
 	"net/http"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/handlers"
 )
 func main(){
 	r := mux.NewRouter()
+	methods := handlers.AllowedHeaders([]string{"GET","POST","PUT","DELETE"})
+	origins := handlers.AllowedOrigins([]string{"*"})
 	r.HandleFunc("/",handler).Methods("GET")
 	r.HandleFunc("/about",aboutHandler).Methods("GET")
 	r.HandleFunc("/contact",contactHandler).Methods("GET")
 	r.HandleFunc("/login",loginHandler).Methods("GET")
-
 	http.Handle("/",r)
-	http.ListenAndServe(":8080",nil)
+	http.ListenAndServe(":8080", handlers.CORS(methods,origins)(r))
 }
 func handler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w,"Home")
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte("{\"Home\": \"Page\"}"))
 }
 func aboutHandler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w,"About")
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte("{\"About\": \"Page\"}"))
 }
 func contactHandler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w,"Contact")
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte("{\"Contact\": \"Page\"}"))
 }
 func loginHandler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w,"Login")
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte("{\"Login\": \"Page\"}"))
 }
-
